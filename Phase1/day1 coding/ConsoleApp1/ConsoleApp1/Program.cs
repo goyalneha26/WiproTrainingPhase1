e31c63df1f39fb315c34256af420190a8cf97296
goyalneha26 <156572197+goyalneha26@users.noreply.github.com>
2025-03-17 13:04:08 +0530

    Add files via upload

---
+++ Phase1/day1 coding/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+class Program
+
+{
+
+     static void Main(string[] args)
+
+     {
+        int i = 1, j = 16;
+
+   while (++i <= j--)
+
+   {
+
+      j++;
+
+   }
+
+   Console.WriteLine(i + "  " + j);
+        //  Program ctrl = new Program();
+
+        //  ctrl.FindOutput();       
+
+     }
+
+ 
+
+     void FindOutput ()
+
+     {
+
+         int num;
+
+         for (num = 10; num <= 15; num++)
+
+         {
+
+             while (Convert.ToBoolean(num))
+
+             {
+
+                 do
+
+                 {
+
+                     Console.WriteLine(1);
+
+                     if (!Convert.ToBoolean(num >> 1))
+
+                         break;
+
+                 } while (Convert.ToBoolean(1));
+
+                 break;
+
+             }
+
+         }
+
+     }
+
+}
---
+++ Phase1/day1 coding/testapp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace mypackage
+{
+  class CsharpProgram{
+    public static void Main(string[] args) {
+Console.WriteLine("please enter the length of the rectangle");
+int length = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("please enter the breadth of the rectangle");
+int breadth = Convert.ToInt32(Console.ReadLine());
+int hfhd=34;
+      
+Console.WriteLine("area of rectangle is equal to "+length*breadth);
+Console.WriteLine("area of rectangle is equal to "+@hfhd);
+
+    }
+  }
+}
